fc1d34a8cb19bd8e45d9dd7631361b2b83362759
CodeofGuff <guffcode@gmail.com>
2024-07-17 20:36:43 -0700

    Started!

---
+++ tarotReaderV1/tarotReaderV1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tarotReaderV1
+//
+//  Created by David Guffre on 7/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tarotReaderV1/tarotReaderV1/tarotReaderV1App.swift
@@ -0,0 +1,17 @@
+//
+//  tarotReaderV1App.swift
+//  tarotReaderV1
+//
+//  Created by David Guffre on 7/17/24.
+//
+
+import SwiftUI
+
+@main
+struct tarotReaderV1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
